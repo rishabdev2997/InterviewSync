feb66a456d1def0f1d3067c0ba7c956698a0ccf1
Rishabdev <rishabofficial2997@gmail.com>
2025-04-05 18:43:11 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,19 @@
+export default {
+  providers: [
+    {
+      domain: "https://clear-coyote-14.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
+
+
+// NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_Y2xlYXItY295b3RlLTE0LmNsZXJrLmFjY291bnRzLmRldiQ
+// CLERK_SECRET_KEY=sk_test_6PVn7GKifde7gqSsVCNA8f18iq6Vor4zlhaEfVWE8U
+
+// # Deployment used by `npx convex dev`
+// CONVEX_DEPLOYMENT=dev:gallant-sandpiper-993 # team: rishabdev-tudu, project: interviewsync-a788e
+
+// NEXT_PUBLIC_CONVEX_URL=https://gallant-sandpiper-993.convex.cloud
+// NEXT_PUBLIC_STREAM_API_KEY=hsfjehme996c
+// STREAM_SECRET_KEY=rjrpbknbe2yktqrnmwh6anajnqb3pcru95hvftphqx52zukxfs5qztuvgr3n27jr
